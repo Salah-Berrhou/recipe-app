316876ccca7317cbb82ac2a1f091c411b41a65a9
unknown <you@example.com>
2019-04-26 15:36:06 +0100

    Final version

--- app.js
+++ app.js
@@ -1,55 +1,86 @@
 'use strict'
+// to get the id of the recipe we are editing so we will used it every time we need to edit anything
+let recipeID = recipes.findIndex((recipe) => {
+    return recipe.id === location.hash.substring(1)
+})
+// to get the values of the rcipe and show them in the edit panel
+document.querySelector('#recipeTitle').value = recipes[recipeID].title
+document.querySelector('#recipeSteps').value = recipes[recipeID].description
 
-
+// save recipes in local storeg
 document.querySelector('#save').addEventListener('click', (e) => {
     let recipeTitle = document.querySelector('#recipeTitle').value 
     let recipeDescription = document.querySelector('#recipeSteps').value 
 
-    let recipe = recipes.find((recipe) =>{
-        if(recipe.id === location.hash.substring(1)){
-            return true
-        }else{
-            return false
-        }
-    })
+    recipes[recipeID].title = recipeTitle !== '' ? recipeTitle : 'Untitled'
+    recipes[recipeID].description = recipeDescription
 
-    if(recipe){
-        let index = recipes.findIndex((recipe) => {
-            return recipe.id === location.hash.substring(1)
-        })
-        recipes[index].title = recipeTitle
-        recipes[index].description = recipeDescription
-    }else{
-        recipes.push({
-            id: location.hash.substring(1),
-            title: recipeTitle !== null? recipeTitle: `Untitled`,
-            description: recipeDescription,
-            ingrediant: [],
-        })
-    }
-    console.log(recipes)
     localStorage.setItem('recipes', JSON.stringify(recipes))
+    console.log(recipes)
 })
-
+// adding ingrediant and rendering them
 document.querySelector('#ingrediant-form').addEventListener('submit', (e) => {
     e.preventDefault()
-    let index = recipes.findIndex((recipe) => {
-        return recipe.id === location.hash.substring(1)
-    })
-    recipes[index].ingrediant.push(e.target[0].value)
-    e.target[0].value = ''
-    console.log(recipes)
-    ingrediantRender(recipes)
+    if (e.target[0].value !== ''){
+        recipes[recipeID].ingrediant.push({
+            id: uuidv4(),
+            title: e.target[0].value,
+            completed: false,
+        })
+        e.target[0].value = ''
+        ingrediantRender(recipes, recipeID)
+        localStorage.setItem('recipes', JSON.stringify(recipes))
+    }
 })
 
-const ingrediantRender = (recipes) => {
-    let index = recipes.findIndex((recipe) => {
-        return recipe.id === location.hash.substring(1)
-    })
+// the building block of the ingrediant
+const ingrediantRender = (recipes, recipeID) => {
     document.querySelector('#ingrediant-container').innerHTML = ''
-    recipes[index].ingrediant.forEach(ingrediant => {
+    recipes[recipeID].ingrediant.forEach(ingrediant => {
+        let ingrediantBox = document.createElement('div')
+        let ingrediantCheacked = document.createElement('input')
         let ingrediantName = document.createElement('p')
-        ingrediantName.textContent = ingrediant
-        document.querySelector('#ingrediant-container').appendChild(ingrediantName)
+        let ingrediantRemove = document.createElement('a')
+
+        ingrediantCheacked.setAttribute('type', 'checkbox')
+        ingrediantCheacked.checked = ingrediant.completed
+        ingrediantBox.appendChild(ingrediantCheacked)
+        ingrediantCheacked.addEventListener('change', (e) => {
+            ingrediant.completed = e.target.checked
+            if(ingrediant.completed){
+                ingrediantName.classList.toggle('cheaked')
+            }else{
+                ingrediantName.classList.remove('cheaked')
+            }
+            localStorage.setItem('recipes', JSON.stringify(recipes))
+            console.log(ingrediant)
+        })
+
+        ingrediantRemove.textContent = 'Remove'
+        ingrediantRemove.addEventListener('click', (e) => {
+            removeIngrediant(ingrediant.id)
+            ingrediantRender(recipes, recipeID)
+            localStorage.setItem('recipes', JSON.stringify(recipes))
+
+        })
+        ingrediantName.textContent = ingrediant.title
+
+        ingrediantBox.appendChild(ingrediantName)
+        ingrediantBox.appendChild(ingrediantRemove)
+        document.querySelector('#ingrediant-container').appendChild(ingrediantBox)
     });
 }
+
+// to remove the recipe
+document.querySelector('#remove').addEventListener('click', function(e){
+    recipes.splice(recipeID, 1)
+    localStorage.setItem('recipes', JSON.stringify(recipes))
+    location.assign('index.html')
+})
+
+const removeIngrediant = (id) => {
+    let index = recipes[recipeID].ingrediant.findIndex((ingrediant) => ingrediant.id === id)
+    recipes[recipeID].ingrediant.splice(index, 1)
+}
+
+ingrediantRender(recipes, recipeID)
--- main.js
+++ main.js
@@ -1,15 +1,48 @@
 // recipes = getSavedRecipes()
 
-document.querySelector('#addRecipe').addEventListener('click', (e) => {
-    location.assign(`edit.html#${uuidv4()}`)
-})
-
-let renderRecipes = (recipes) => {
-    recipes.forEach(recipe => {
-        let recipeTitle = document.createElement('h2')
-        recipeTitle.textContent = recipe.title 
-        document.querySelector('#recipes-container').appendChild(recipeTitle)
-    });
+let filter = {
+    text: '',
 }
 
-renderRecipes(recipes)
+document.querySelector('#addRecipe').addEventListener('click', (e) => {
+    let recipeID = uuidv4()
+    recipes.push({
+    id: recipeID,
+    title: '' ,
+    description: '' ,
+    ingrediant: [],
+    })
+    localStorage.setItem('recipes', JSON.stringify(recipes))
+    location.assign(`edit.html#${recipeID}`)
+})
+
+document.querySelector('#search-filter').addEventListener('input', (e) => {
+    filter.text = e.target.value
+    renderRecipes(recipes, filter)
+})
+
+let renderRecipes = (recipes, filter) => {
+    let filterdResipes = recipes.filter((recipe) => {
+        return recipe.title.toLowerCase().includes(filter.text.toLowerCase())
+    })
+    document.querySelector('#recipes-container').innerHTML = ''
+    filterdResipes.forEach(recipe => {
+        let recipeBox = document.createElement('div')
+        let recipeTitle = document.createElement('h2')
+        recipeTitle.textContent = recipe.title 
+
+        let recipeDescription = document.createElement('p')
+        recipeDescription.textContent = `${recipe.description.slice(0, 160)} ...`
+        
+        recipeBox.appendChild(recipeTitle)
+        recipeBox.appendChild(recipeDescription)
+        document.querySelector('#recipes-container').appendChild(recipeBox)
+
+        recipeBox.addEventListener('click', (e) => {
+            location.assign(`edit.html#${recipe.id}`)
+        })
+    });
+    console.log(recipes)
+}
+
+renderRecipes(recipes, filter)
